ff42c184c7b9fc4df9d6991a07504453481fbd0c
Lanslk <wolf810210@hotmail.com>
2024-10-04 13:51:46 +1000

    Crate project Add audio

---
+++ PacManOrcaAssessment/Assets/Scripts/AudioSequencePlayer.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class AudioSequencePlayer : MonoBehaviour
+{
+    private AudioSource[] audioSources;
+    private int currentAudioIndex = 0;
+
+    void Start()
+    {
+        // Get all AudioSources attached to this GameObject
+        audioSources = GetComponents<AudioSource>();
+
+        foreach (var audioSource in audioSources)
+        {
+            audioSource.Stop();
+        }
+        
+        // Play the first audio clip
+        if (audioSources.Length > 0)
+        {
+            PlayAudioClip(currentAudioIndex);
+        }
+    }
+
+    void Update()
+    {
+        // Check if the current clip is no longer playing
+        if (!audioSources[currentAudioIndex].isPlaying)
+        {
+            // Move to the next audio clip
+            currentAudioIndex++;
+            
+            // Play the next clip if available
+            if (currentAudioIndex < audioSources.Length)
+            {
+                PlayAudioClip(currentAudioIndex);
+            }
+        }
+    }
+
+    void PlayAudioClip(int index)
+    {
+        if (index >= 0 && index < audioSources.Length)
+        {
+            audioSources[index].Play();
+        }
+    }
+}
